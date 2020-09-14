465188e2ee7e8800f1dde1c2542371e32bfbf5c1
thai <tathai2610@gmail.com>
2020-09-14 12:39:31 +0700

    1

--- db/migrate/20200910094003_remove_albums_in_photos.rb
+++
@@ -1,6 +0,0 @@
-class RemoveAlbumsInPhotos < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :photos, :albums_id
-    add_reference :photos, :album, foreign_key: true, optional: true
-  end
-end
